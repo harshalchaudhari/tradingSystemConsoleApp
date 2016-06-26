cd62b3fe836362e2882748990737633e30e728bb
Kunal Patil <kupatil@microsoft.com>
2016-06-25 22:16:29 -0700

    Add StockObserver and TickerHelper classes

---
+++ tradeSystemConsoleApplication/tradeSystemConsoleApplication/StockObserver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket
+{
+    public class StockObserver
+    {
+        private readonly Queue<Dictionary<string, object>> cachedValues;
+
+        public int MaximumCachedValues { get; private set; }
+        public int MinimumVolumeTradedInDayThreshold { get; private set; }
+
+        //TODO: Fix the hard coded values
+        public StockObserver() : this(5, 100000)
+        {}
+
+        public StockObserver(int maximumCachedValues, int minimumVolumeTradedInDayThreshold)
+        {
+            cachedValues = new Queue<Dictionary<string, object>>();
+            MaximumCachedValues = maximumCachedValues;
+            MinimumVolumeTradedInDayThreshold = minimumVolumeTradedInDayThreshold;
+        }
+    }
+}
---
+++ tradeSystemConsoleApplication/tradeSystemConsoleApplication/TickerHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket
+{
+    public static class TickerHelper
+    {
+        public static IEnumerable<string> GetTickers(string tickerFilePath, char[] delimiters)
+        {
+            return File.ReadAllText(tickerFilePath).Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static IEnumerable<string> GetConcatenatedTickers(string tickerFilePath, char[] delimiters, char separater, int maxTickersToConcatenate)
+        {
+            return GetConcatenatedTickers(GetTickers(tickerFilePath, delimiters), separater, maxTickersToConcatenate);
+        }
+
+        public static IEnumerable<string> GetConcatenatedTickers(IEnumerable<string> tickers, char separator, int maxTickersToConcatenate)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<string> concatenatedTickers = new List<string>();
+            int count = 0;
+
+            foreach (string ticker in tickers)
+            {
+                if (count++ == maxTickersToConcatenate)
+                {
+                    concatenatedTickers.Add(sb.ToString().TrimEnd(separator));
+                    sb = new StringBuilder();
+                    count = 1;
+                }
+                sb.Append(ticker + separator);
+            }
+
+            if (sb.Length > 0)
+            {
+                concatenatedTickers.Add(sb.ToString().TrimEnd(separator));
+            }
+            
+            return concatenatedTickers;
+        }
+    }
+}
