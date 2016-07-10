2ff82b8f2f3a043d289868286183f1957ff810dd
Kunal Patil <kp101090@gmail.com>
2016-07-10 13:59:02 -0700

    Add basic unit test skeleton

---
+++ tradeSystemConsoleApplication/TradeSystemConsoleApplication.UnitTests/StockObserverUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TradeSystemConsoleApplication.UnitTests
+{
+    [TestClass]
+    public class StockObserverUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
